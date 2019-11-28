d43140983bbf88db0964fdf977ee9c3b7802fd4c
Claire Marchal <clairemarchal@mbp-de-claire.lan>
2019-11-28 15:55:58 +0100

    profileScreen ok avant map

--- containers/ProfileScreen.js
+++ containers/ProfileScreen.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useRoute } from "@react-navigation/core";
-import { Text, View } from "react-native";
+import { Text, View, ActivityIndicator, ScrollView } from "react-native";
 import axios from "axios";
+import RoomCard from "../components/RoomCard";
+import MapView from "react-native-maps";
 
 export default function ProfileScreen() {
   const { params } = useRoute();
 
   const [isLoading, setIsLoading] = useState(true);
   const [room, setRoom] = useState({});
+  const [isDescriptionDisplayed, setIsDescriptionDisplayed] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,11 +21,12 @@ export default function ProfileScreen() {
 
         if (response.data) {
           setRoom(response.data);
+          setIsLoading(false);
         } else {
-          //à mettre pour différencier : si ID n'existe pas
+          //type erreur = ID n'existe pas
           alert("Id non trouvé");
         }
-        //à mettre pour différencier pb serveur (éteind, Api qui fonctionne pas...)
+        //type erreur = pb serveur (éteind, Api qui ne fonctionne pas...)
       } catch (error) {
         console.log(error);
         alert("An error occurred");
@@ -33,10 +37,46 @@ export default function ProfileScreen() {
   }, []);
 
   return (
-    <>
-      <View>
-        <Text>{room.description}</Text>
-      </View>
-    </>
+    <ScrollView>
+      {isLoading === true ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size="large" color="red" />
+        </View>
+      ) : (
+        <RoomCard item={room}></RoomCard>
+      )}
+
+      <Text
+        onPress={() => {
+          // ! permet d'inverser le boolean
+          setIsDescriptionDisplayed(!isDescriptionDisplayed);
+        }}
+        style={{ marginHorizontal: 20 }}
+        numberOfLines={isDescriptionDisplayed === false ? 2 : 0}
+      >
+        {room.description}
+      </Text>
+
+      {/* <MapView
+        showsUserLocation={false}
+        // provider="google"
+        style={{ height: 300, marginTop: 50 }}
+        initialRegion={{
+          latitude: room.loc[1],
+          longitude: room.loc[0],
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.03
+        }}
+      >
+        <MapView.Marker
+          coordinate={{
+            latitude: room.loc[1],
+            longitude: room.loc[0]
+          }}
+        />
+      </MapView> */}
+    </ScrollView>
   );
 }
